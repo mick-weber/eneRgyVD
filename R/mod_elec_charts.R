889e69d180d97ff4e8127f9ff4a1d06cfcd1c143
mwr <michael.weber@vd.ch>
2022-06-14 15:39:48 +0200

    removed data download module due to blocked reactivity issue

--- R/mod_elec_charts.R
+++ R/mod_elec_charts.R
@@ -80,7 +80,17 @@ mod_elec_charts_ui <- function(id){
                              # breathing
                              br(),
                              # Download module
-                             mod_download_data_ui(ns("table_download")),
+                             #mod_download_data_ui(ns("table_download")),
+
+                             # test unmodule
+                             h4("Télécharger les données"),
+                             shiny::downloadButton(outputId = ns("download_csv"),
+                                                   label = "CSV", class = "dlButton"),
+                             shiny::downloadButton(outputId = ns("download_excel"),
+                                                   label = "XLSX", class = "dlButton" ), # class defined in custom.css
+
+                             # / test unmodule
+
                              # breathing
                              br(),
                              # DT table
@@ -170,9 +180,30 @@ mod_elec_charts_server <- function(id,
       }# End else if
     })# End observe
 
-    mod_download_data_server("table_download",
-                             data = subsetData(),
-                             dl_prefix = dl_prefix) # dl preffix for file name, passed into app_server.R
+    # mod_download_data_server("table_download",
+    #                          data = subsetData(),
+    #                          dl_prefix = dl_prefix) # dl preffix for file name, passed into app_server.R
+
+
+    # test unmodule
+
+    # CSV handler
+    output$download_csv <- downloadHandler(
+      filename = paste0(dl_prefix, Sys.Date(), ".csv"),
+      content = function(file){
+        readr::write_excel_csv2(subsetData(), file = file) # https://www.rdocumentation.org/packages/readr/versions/1.3.0/topics/write_delim
+      }
+    )
+    # XLSX handler
+    output$download_excel <- downloadHandler(
+      filename = paste0(dl_prefix, Sys.Date(), ".xlsx"),
+      content = function(file){
+        writexl::write_xlsx(subsetData(), path = file)
+      }
+    )
+
+    # / test unmodule
+
 
 
     output$table_1 <- DT::renderDataTable({

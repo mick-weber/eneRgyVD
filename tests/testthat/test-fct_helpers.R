835ceb295b8037ea7ee9d93a8f83b933c2121319
Weber Michaël <michaelweber_cnp@outlook.com>
2024-03-21 13:58:12 +0100

    more tests

--- tests/testthat/test-fct_helpers.R
+++ tests/testthat/test-fct_helpers.R
@@ -1,15 +1,13 @@
+# sf_layers ----
 test_that("Check that map layers are valid components for a leaflet map",
           {
 
             # Most basic map with the app 3 layers
-            map <- leaflet::leaflet() |>
-              # Couche de base des districts si un district est sélectionné
-              leaflet::addPolygons(data = sf_districts) |>
-              # Couche des lacs
-              leaflet::addPolygons(data = sf_lacs) |>
-              # Première couche des communes (en blanc, état non-sélectionné)
-              leaflet::addPolygons(data = sf_communes)
-
+            map <- create_select_leaflet(
+              sf_districts = eneRgyVD::sf_districts,
+              sf_lacs = eneRgyVD::sf_lacs,
+              sf_communes = eneRgyVD::sf_communes
+            )
             # Check class
             expect_equal(class(map),
                          c("leaflet", "htmlwidget"))
@@ -17,4 +15,34 @@ test_that("Check that map layers are valid components for a leaflet map",
           })
 
 
+# convert_units() ----
+test_that("Check unit conversion works as expected",
+          {
+
+            # Actual values
+            actual_values <- c(
+             eneRgyVD::convert_units(data = data.frame(kwh = 1e3),colnames = "kwh", unit_from = "kWh", unit_to = "kWh")[[1]],
+             eneRgyVD::convert_units(data = data.frame(kwh = 1e3),colnames = "kwh", unit_from = "kWh", unit_to = "MWh")[[1]],
+             eneRgyVD::convert_units(data = data.frame(kwh = 1e3),colnames = "kwh", unit_from = "kWh", unit_to = "GWh")[[1]],
+             eneRgyVD::convert_units(data = data.frame(kwh = 1e3),colnames = "kwh", unit_from = "kWh", unit_to = "TJ")[[1]]
+            )
+
+            # Expected values
+            expected_values <- c(1e3, 1, 1e-3, 3.6e-3)
+
+            # Test
+            expect_equal(object = actual_values, expected = expected_values)
+
+          }
+)
+
+# add_colname_units() ----
+test_that("Check unit addition in colname works as expected",
+          {
+            df_in <- eneRgyVD::add_colname_units(data = data.frame(consommation = 100), unit = "MWh")
+            df_out <- data.frame(`consommation [MWh]` = 100, check.names = FALSE)
+
+            expect_equal(df_in, df_out)
+          }
+)
 

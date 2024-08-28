56cdc20b1a3ffeb38c105c1fe01c3ff3599d1488
Weber Michaël <michaelweber_cnp@outlook.com>
2024-08-28 11:55:47 +0200

    removed theme code from utils_helpers.R (now in theme.R)

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -457,36 +457,6 @@ subsidies_measure_icons <- subsidies_measure_palette_table |>
 #   setNames(nm = subsidies_measure_palette_table$mesure)
 
 
-# Theme ----
-
-## Color used for multiple ui items ----
-
-main_color <- "#3A862D"
-main_color_active <- "#343A40"
-
-## Custom {fresh} theme passed to bs4Dash in app_ui.R
-# Example from https://dreamrs.github.io/fresh/
-
-profil_theme <- bslib::bs_theme(version = 5,
-                                preset = "bootstrap",
-                                font_scale = 1.2,
-                                "navbar-bg" = "#3A862D",
-                                "modal-footer-margin-between" = "0rem",
-                                primary = "#3A862D",
-                                secondary = "#343A40"
-) |>
-  # add some variables
-  bslib::bs_add_variables(
-    "dropdown-link-active-bg" = "white",         # nav menus
-    "dropdown-link-active-color" = "$secondary", # nav menus
-    "accordion-button-active-bg" = "$secondary", # bg color when activated
-    "accordion-button-active-color" = "white",   # text color when activated
-    "accordion-button-focus-box-shadow" = "0 0 0 $btn-focus-width rgba($secondary, 0.25)", # width + color of shadow when active
-
-    .where = "declarations") |>
-    bslib::bs_add_rules(sass::sass_file("./inst/app/www/custom_bs5.scss")) # add complementary sass file
-
-
 # Non-reactive objects for input widgets ----
 
 ## Generic objects (across all tabs) ----

53d3d942678406d694a9197d53dfee2d26061212
Weber Michaël <michaelweber_cnp@outlook.com>
2024-12-02 09:44:48 +0100

    update mod_news.R with profil climatique release

--- R/mod_news.R
+++ R/mod_news.R
@@ -17,7 +17,7 @@ mod_news_ui <- function(id){
                           h4(strong("Historique des versions")),
                           br(),
 
-                          bslib::accordion(open = c("tocome", "oct24"), # refers to 'value' of accordion_panel. !!update when adding entry!!!!
+                          bslib::accordion(open = c("tocome", "jan25"), # refers to 'value' of accordion_panel. !!update when adding entry!!!!
                                            multiple = TRUE,
 
                                            # Next updates ----
@@ -25,12 +25,38 @@ mod_news_ui <- function(id){
                                                                   value = "tocome",
                                                                   icon = bsicons::bs_icon("question-circle", size = "1.5rem"),
 
+                                                                  HTML(bsicons::bs_icon("three-dots", size = "1.5rem"),
+                                                                       "Processus de mise à jour des données publiées durant 2025")
+                                           ),
+
+                                           # Janvier 2025
+
+                                           bslib::accordion_panel(title = HTML("<strong>Janvier 2025</strong>"),
+                                                                  value = "jan25",
+                                                                  icon = bsicons::bs_icon("calendar-check", size = "1.5rem"),
+                                                                  HTML(bsicons::bs_icon("star", size = "1.5rem"),
+                                                                       "Migration profil climatique"),
+                                                                  br(),br(),
                                                                   HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
                                                                        "Ajout des données de distribution de gaz 2018-2022"),
                                                                   br(),br(),
-                                                                  HTML(bsicons::bs_icon("star", size = "1.5rem"),
-                                                                       "Migration vers un profil climatique (décembre 2024-janvier 2025)")
+                                                                  HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
+                                                                       "Ajout des données de surface de canopée en zone urbaine"),
+                                                                  br(),br(),
+                                                                  HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
+                                                                       "Ajout des données des bâtiments exposés à des dangers naturels"),
+                                                                  br(),br(),
+                                                                  HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
+                                                                       "Ajout des données de qualité de zone de desserte des transports publics"),
+                                                                  br(),br(),
+                                                                  HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
+                                                                       "Ajout des données de part de véhicules électriques"),
+                                                                  br(),br(),
+                                                                  HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
+                                                                       "Ajout des données de taux de motorisation")
                                            ),
+
+                                           # Octobre 2024
 
                                            bslib::accordion_panel(title = HTML("<strong>Octobre 2024</strong>"),
                                                                   value = "oct24",
@@ -74,20 +100,17 @@ mod_news_ui <- function(id){
                                                                   value = "june23",
                                                                   icon = bsicons::bs_icon("calendar-check", size = "1.5rem"),
 
+                                                                  HTML(bsicons::bs_icon("star", size = "1.5rem"),
+                                                                       "Mise en ligne du profil énergétique"),
+                                                                  br(),br(),
                                                                   HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
                                                                        "Ajout des données chaleur bâtiments 2022 (méthode 2023)"),
                                                                   br(),br(),
                                                                   HTML(bsicons::bs_icon("database-fill-add", size = "1.5rem"),
-                                                                       "Ajout des données de production d'électricité 2015-2022"),
-                                                                  br(),br(),
-                                                                  HTML(bsicons::bs_icon("star", size = "1.5rem"),
-                                                                       "Mise en ligne du profil énergétique")
+                                                                       "Ajout des données de production d'électricité 2015-2022")
                                            )
-                          )
+                          )# End accordion
     )# End layout_columns
-
-
-
   )
 }
 

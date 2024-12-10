841c4b8f17235abc9d127c13b7cc64d5b6c6ff04
Weber Michaël <michaelweber_cnp@outlook.com>
2024-12-10 16:42:30 +0100

    badge to stable for next release

--- dev/01_start.R
+++ dev/01_start.R
@@ -38,7 +38,7 @@ usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
 usethis::use_code_of_conduct(contact = "Golem User")
-usethis::use_lifecycle_badge("Experimental")
+usethis::use_lifecycle_badge("stable")
 usethis::use_news_md(open = FALSE)
 
 ## Use git ----

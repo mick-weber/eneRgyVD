ea4c30cdc568b6a8d17acad8d6e0fdb1fd009306
lgnbhl <felix.luginbuhl@protonmail.ch>
2025-02-20 10:05:08 +0000

    fix global variables notes

---
+++ R/globals.R
@@ -0,0 +1,2 @@
+globalVariables(c(":=", "N_EGID", "SRE", "annee", "commune", "etat", "mesure_simplifiee", "nombre", "paragraph",
+                  "prop", "rename_column_output", "section_header", "stratum", "subv_type", "total"))

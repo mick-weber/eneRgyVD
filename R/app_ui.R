5e7830c023d15e4a073896304a79923a3cd22d9b
mick-weber <h7q1n0@vd.ch>
2025-04-17 13:12:27 +0200

    meta tag description added for web SEO

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,6 +14,10 @@ app_ui <- function(request) {
     ## |---------------------------------------------------------------|
     # First proceed to load JS scripts / librairies as needed
     tags$head(
+      # Add description for web content
+      tags$meta(name = "description",
+                content = "Découvrez la situation énergétique et climatique de votre commune à l'aide de données mises à disposition par le Canton de Vaud."
+                ),
 
       # Add introJS library for the guided tour
       tags$link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/intro.js/minified/introjs.min.css"),
